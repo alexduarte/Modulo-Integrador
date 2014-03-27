6b7695527c05fe6c123357d578f149a6524bbe73
kennedimalheiros <kennedimalheiros@gmail.com>
2014-03-27 15:45:19 -0300

    Commit Inicial

---
+++ src/br/com/atsinformatica/erp/entity/EnderecoEcomBean.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.atsinformatica.erp.entity;
+
+/**
+ *
+ * @author kennedimalheiros
+ */
+public class EnderecoEcomBean {
+
+    private String id_customer;
+    private String alias;
+    private String lastname;
+    private String firstname;
+    private String address1;
+    private String address2;
+    private String postcode;
+    private String city;
+    private String other;
+    private String phone;
+    private String phone_mobile;
+
+    public String getId_customer() {
+        return id_customer;
+    }
+
+    public void setId_customer(String id_customer) {
+        this.id_customer = id_customer;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone_mobile() {
+        return phone_mobile;
+    }
+
+    public void setPhone_mobile(String phone_mobile) {
+        this.phone_mobile = phone_mobile;
+    }
+    
+    
+}
