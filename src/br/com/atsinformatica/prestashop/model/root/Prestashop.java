d03c5d2484b984a930441d358a92cfa13bf6de8f
kennedimalheiros <kennedimalheiros@gmail.com>
2014-03-27 15:37:40 -0300

    Adicionei os propOrder "customer", "address", "order", "cpfmodule_data

--- src/br/com/atsinformatica/prestashop/model/root/Prestashop.java
+++ src/br/com/atsinformatica/prestashop/model/root/Prestashop.java
@@ -1,7 +1,5 @@
 package br.com.atsinformatica.prestashop.model.root;
 
-
-
 import br.com.atsinformatica.prestashop.model.node.Content;
 import javax.xml.bind.annotation.*;
 
@@ -9,7 +7,6 @@ import javax.xml.bind.annotation.*;
  *
  * @author ricardosilva
  */
-
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(propOrder = {
     "productFeature",
@@ -17,34 +14,45 @@ import javax.xml.bind.annotation.*;
     "product",
     "image",
     "content",
-    
-})
+    "customer",
+    "address",
+    "order",
+    "cpfmodule_data",})
 @XmlRootElement(name = "prestashop")
-public class Prestashop{
-    
+public class Prestashop {
+
     @XmlAttribute(name = "xmlns:xlink", required = true)
     protected String xmlns = "http://www.w3.org/1999/xlink";
-    
-    @XmlElement(name = "product_feature",required = true)
+
+    @XmlElement(name = "product_feature", required = true)
     protected ProductFeature productFeature;
-    
-    @XmlElement(name = "category",required = true)
+
+    @XmlElement(name = "category", required = true)
     protected Category category;
-    
-    @XmlElement(name = "product",required = true)
+
+    @XmlElement(name = "product", required = true)
     protected Product product;
-    
-    @XmlElement(name = "image",required = true)
+
+    @XmlElement(name = "image", required = true)
     private Image image;
-    
-    @XmlElement(name = "content",required = true)
+
+    @XmlElement(name = "content", required = true)
     private byte[] content;
+
+    @XmlElement(name = "customer", required = true)
+    protected Customer customer;
+
+    @XmlElement(name = "address", required = true)
+    protected Address address;
+    @XmlElement(name = "order", required = true)
+    protected Order order;
+
+    @XmlElement(name = "cpfmodule_data", required = true)
+    protected CPFModuleData cpfmodule_data;
 
     public ProductFeature getProductFeature() {
         return productFeature;
     }
-    
-    
 
     public void setProductFeature(ProductFeature value) {
         this.productFeature = value;
@@ -106,5 +114,37 @@ public class Prestashop{
     public void setContent(byte[] content) {
         this.content = content;
     }
-}
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public CPFModuleData getCpfmodule_data() {
+        return cpfmodule_data;
+    }
+
+    public void setCpfmodule_data(CPFModuleData cpfmodule_data) {
+        this.cpfmodule_data = cpfmodule_data;
+    }
+
+}
