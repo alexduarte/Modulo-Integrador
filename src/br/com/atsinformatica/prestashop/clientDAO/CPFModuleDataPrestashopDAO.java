617672fc32fc6e0f7601ab509adc0f2ac5b9b734
kennedimalheiros <kennedimalheiros@gmail.com>
2014-03-27 15:42:44 -0300

    Commit Inicial

---
+++ src/br/com/atsinformatica/prestashop/clientDAO/CPFModuleDataPrestashopDAO.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.atsinformatica.prestashop.clientDAO;
+
+import br.com.atsinformatica.prestashop.model.root.CPFModuleData;
+import br.com.atsinformatica.prestashop.model.root.Prestashop;
+import java.util.List;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author kennedimalheiros
+ */
+public class CPFModuleDataPrestashopDAO extends GenericPrestashopDAO<CPFModuleData> implements IGenericPrestashopDAO<CPFModuleData> {
+
+    @Override
+    public void post(String path, CPFModuleData t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void put(String path, CPFModuleData t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<CPFModuleData> get(String path) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public CPFModuleData getId(String path, int key) {
+
+        Prestashop prestashop = getWebResource().path(path).path(String.valueOf(key)).type(MediaType.APPLICATION_XML).get(Prestashop.class);
+        return prestashop.getCpfmodule_data();
+
+    }
+
+    @Override
+    public void delete(String path, String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
