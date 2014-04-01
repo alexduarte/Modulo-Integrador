45f9ea8e8256aad5c2678841ebb50dacd5a5b476
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-01 08:56:16 -0300

    Commit Inicial

---
+++ src/br/com/atsinformatica/erp/entity/EstadoERPBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.atsinformatica.erp.entity;
+
+/**
+ *
+ * @author kennedimalheiros
+ */
+public class EstadoERPBean {
+
+    private String id;
+    private String sigla;
+    private String descricao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
