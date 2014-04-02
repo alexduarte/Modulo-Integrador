653aedbc1485d895a82c6bfc57a0f466680591c1
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-02 14:45:03 -0300

    Removendo a função ultimoRegistro, foi alterado a forma de gerar o cod do pedido.
    Criado metodos para verificar os parametros que implica no incremento do numero do Pedido.
    parametroUsaDAVPDV, parametroParaCadUsarGenPDV, parametroParaFat, erpProximoCodGenPDV, erpGenCodPedido, 
    erpPedidoCExiste, erpMaxNpdv, erpUtilizaECF
    
    Por fim depois de verificar todos os parametros, vai ser gerado o código do pedido.
    Função que gera código do pedido (gerarCodPedido)

--- src/br/com/atsinformatica/erp/dao/PedidoERPDAO.java
+++ src/br/com/atsinformatica/erp/dao/PedidoERPDAO.java
@@ -5,17 +5,15 @@
  */
 package br.com.atsinformatica.erp.dao;
 
-import br.com.atsinformatica.erp.entity.EstadoERPBean;
 import br.com.atsinformatica.erp.entity.PedidoERPBean;
 import br.com.atsinformatica.midler.jdbc.ConexaoATS;
 import br.com.atsinformatica.utils.Funcoes;
-import br.com.atsinformatica.utils.Log;
+import br.com.atsinformatica.utils.LogERP;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 import org.apache.log4j.Logger;
 
@@ -55,25 +53,7 @@ public class PedidoERPDAO implements IGenericDAO<PedidoERPBean> {
 
     @Override
     public String ultimoRegistro() throws SQLException {
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            String sql = "select max(CODPEDIDO) cod from PEDIDOC";
-            stmt = ConexaoATS.conectaERP().createStatement();
-            rs = stmt.executeQuery(sql);
-            int cod = 0;
-            while (rs.next()) {
-                if (rs.getString("cod") != null) {
-                    cod = Integer.valueOf(rs.getString("cod")) + 1;
-                }
-            }
-            return Funcoes.preencheCom(Integer.toString(cod), "0", 8, Funcoes.LEFT);
-        } catch (Exception e) {
-            return null;
-        } finally {
-            stmt.close();
-            rs.close();
-        }
+        return null;
     }
 
     /**
@@ -110,35 +90,336 @@ public class PedidoERPDAO implements IGenericDAO<PedidoERPBean> {
         }
     }
 
-    public void gravarPedido(PedidoERPBean pedidoERPBean, String codClienteERP) throws SQLException {
+    public boolean gravarPedido(PedidoERPBean pedidoERPBean, String codClienteERP) throws SQLException {
+        String codPedido;
         PreparedStatement pstmt = null;
+        String codEmpresa = new ParaEcomDAO().listaTodos().get(0).getCodEmpresaEcom();
         try {
 
             conn = ConexaoATS.conectaERP();
-            String sql = " INSERT INTO PEDIDOC (CODPEDIDO, IDPEDIDOECOM, "
-                    + "                      CODCLIENTE, DATAPEDIDO, HORA, FRETE) "
-                    + "              VALUES (?, ?, ?, ?, ?, ?)";
+            String sql = " INSERT INTO PEDIDOC "
+                    + "                    (CODPEDIDO, IDPEDIDOECOM, CODEMPRESA, "
+                    + "                     TIPOPEDIDO, DESCONTOVLR, "
+                    + "                     CODCLIENTE, DATAPEDIDO, HORA, FRETE) "
+                    + "              VALUES(?,?,?,?,?,?,?,?,?)";
+
             pstmt = conn.prepareStatement(sql);
+            codPedido = gerarCodPedido();
+            pstmt.setString(1, codPedido);
+            pstmt.setString(2, pedidoERPBean.getId_ecom());
+            pstmt.setString(3, codEmpresa);
+            pstmt.setString(4, "55");
+            pstmt.setDouble(5, Double.valueOf(pedidoERPBean.getTotal_discounts()));
+            pstmt.setString(6, codClienteERP);
+            pstmt.setDate(7, new Date(pedidoERPBean.getDate_add().getTime()));
+            pstmt.setString(8, pedidoERPBean.getHora());
+            pstmt.setDouble(9, Double.valueOf(pedidoERPBean.getTotal_shipping()));
 
-            pstmt.setString(1, ultimoRegistro());
-            pstmt.setString(2, pedidoERPBean.getId());
-            pstmt.setString(3, codClienteERP);
-            pstmt.setDate(6, new Date(pedidoERPBean.getDate_add().getTime()));
-            pstmt.setString(7, pedidoERPBean.getHora());
-            pstmt.setString(8, pedidoERPBean.getTotal_shipping());
-
-            pstmt.execute();
+            pstmt.executeUpdate();
 
             //Gerando log
-            Log.geraLog("PEDICOC", ultimoRegistro(), "Inclusão", "Incluindo pedido sincronizado do Ecommercer");
-
-        } catch (SQLException e) {
+            LogERP.geraLog("PEDIDOC", ultimoRegistro(), "Inclusão", "Incluindo pedido sincronizado do Ecommercer");
+            return true;
+        } catch (Exception e) {
             logger.error("Erro ao soncronizar Pedido Ecom ( " + ultimoRegistro() + " ): " + e);
-
+            return false;
         } finally {
             conn.close();
             pstmt.close();
         }
+    }
+
+    public String parametroUsaDAVPDV(String codEmpresa) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String retorno = null;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " Select IMPORTAPEDIDO from PARAMECF "
+                    + "                        WHERE CODEMPRESA = ? ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, codEmpresa);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("IMPORTAPEDIDO") != null) {
+                    retorno = rs.getString("IMPORTAPEDIDO");
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na Verificação de parametro UsaDavPDV: " + e);
+            return null;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    /**
+     *
+     * @return True se utiliza UsarGenPDV
+     * @throws SQLException
+     */
+    public boolean parametroParaCadUsarGenPDV() throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean retorno = false;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " SELECT USARGENPDV FROM PARACAD ";
+
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("USARGENPDV") != null) {
+                    retorno = rs.getString("USARGENPDV").equals("S");
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na Verificação de parametro UsarGenPDV: " + e);
+            return false;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    public String parametroParaFat(String campoRetorno) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String retorno = null;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " SELECT " + campoRetorno + " FROM PARAFAT ";
+
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString(campoRetorno) != null) {
+                    retorno = rs.getString(campoRetorno);
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na Verificação de Parametro ParaFat do campo " + campoRetorno + ": " + e);
+            return null;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    public int erpProximoCodGenPDV(String codEmpresa) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int retorno = 0;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " Select Gen_ID(GENPDV" + codEmpresa + ",1) as CodPedido from RDB$DATABASE ";
+
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("CODPEDIDO") != null) {
+                    retorno = rs.getInt("CODPEDIDO");
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na executar função erpProximoCodGenPDV: " + e);
+            return 0;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    public int erpGenCodPedido() throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int retorno = 0;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " select Gen_ID(G_CodPedido_PDV, 1) as Cod from RDB$DATABASE ";
+
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("COD") != null) {
+                    retorno = rs.getInt("COD");
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na executar função erpGenCodPedido: " + e);
+            return 0;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    /**
+     * Se existir pedido com o mesmo codigo TRUE Se não existir pedido com o
+     * mesmo codigo FALSE
+     *
+     * @param codEmpresa
+     * @param tipoPedido
+     * @param codPedido Maximo 8 digitos
+     * @return
+     * @throws SQLException
+     */
+    public boolean erpPedidoCExiste(String codEmpresa, String tipoPedido, String codPedido) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean retorno = false;
+        /**
+         * Passando para 8 digitos para verificar na tabela PEDICOC codPedido =
+         * Funcoes.preencheCom(codPedido, "0", 8, Funcoes.LEFT);
+         */
+        codPedido = Funcoes.preencheCom(codPedido, "0", 8, Funcoes.LEFT);
+
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " SELECT DISTINCT CODPEDIDO                 "
+                    + "                 FROM PEDIDOC                 "
+                    + "                   WHERE                      "
+                    + "                       CODEMPRESA = ?   AND   "
+                    + "                       TIPOPEDIDO  = ?  AND   "
+                    + "                       CODPEDIDO   = ?        ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, codEmpresa);
+            pstmt.setString(2, tipoPedido);
+            pstmt.setString(3, codPedido);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                retorno = rs.getString("CODPEDIDO").equals(codPedido);
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na executar função erpPedidoCExiste: " + e);
+            return false;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    //Se não utiliza UsarGenPDV.
+    public int erpMaxNpdv(String codEmpresa) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        int retorno = 0;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = "  SELECT NUMEROPDV FROM NPDV "
+                    + "           WHERE CODEMPRESA = ? ";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, codEmpresa);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                if (rs.getString("NUMEROPDV") != null) {
+                    retorno = rs.getInt("NUMEROPDV");
+                }
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na executar função erpMaxNpdv: " + e);
+            return 0;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    /**
+     * Verificar se utiliza ECF Vai verificar se tem registro na tabela PDVFisc
+     * se não tiver é porque não utiliza.
+     *
+     * @param codEmpresa
+     * @return True se utilizar ECF
+     * @throws SQLException
+     */
+    public boolean erpUtilizaECF(String codEmpresa) throws SQLException {
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        boolean retorno = false;
+        try {
+            conn = ConexaoATS.conectaERP();
+
+            String sql = " SELECT * FROM PDVFISC P WHERE P.CODEMPRESA = ?";
+
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, codEmpresa);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                retorno = true;
+            }
+            return retorno;
+        } catch (Exception e) {
+            logger.error("Erro na executar função erpSysECF: " + e);
+            return false;
+        } finally {
+            pstmt.close();
+            rs.close();
+        }
+    }
+
+    /**
+     * Função vai validar todo os parametros para verificar qual a rotina é
+     * utilizada para gerado o Código de pedido na tabela PedidoC
+     *
+     * @return Novo código de pedido com 8 digitos.
+     * @throws SQLException
+     */
+    public String gerarCodPedido() throws SQLException {
+        int codPedido = 0;
+        String codEmpresa = new ParaEcomDAO().listaTodos().get(0).getCodEmpresaEcom();
+        if (parametroParaFat("NUMERARPDVAUTO").equals("S")) {
+
+            //Verifica se utiliza ECF
+            if (erpUtilizaECF(codEmpresa)) {
+                codPedido = erpProximoCodGenPDV(codEmpresa);
+
+            } else if (parametroParaCadUsarGenPDV()) { // parametroParaCadUsarGenPDV = S
+                codPedido = erpGenCodPedido();
+
+                //Verificando se o pedido existe
+                if (erpPedidoCExiste(codEmpresa, "55", String.valueOf(codPedido))) {
+                    //Se pedido existe, entra no loop que vai incrementar +1 ate que erpPedidoCExiste seja FALSE
+                    while (!erpPedidoCExiste(codEmpresa, "55", String.valueOf(codPedido))) {
+                        codPedido += 1;
+                    }
+                }
+            } else { //parametroParaCadUsarGenPDV = N
+                codPedido = erpMaxNpdv(codEmpresa);
+
+                //Se codPedido for 0, Adiciona o primeiro cod.
+                if (codPedido == 0) {
+                    codPedido += 1;
+                }
+            }
+        }
+        return Funcoes.preencheCom(String.valueOf(codPedido), "0", 8, Funcoes.LEFT);
     }
 
 }
