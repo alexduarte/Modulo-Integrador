e931fd438fbf278b4e1730567fdf70bcc1336d8c
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-04 18:06:10 -0300

    Adicionando referência do endereço da cobrança.

--- src/br/com/atsinformatica/erp/entity/EnderecoERPBean.java
+++ src/br/com/atsinformatica/erp/entity/EnderecoERPBean.java
@@ -16,6 +16,7 @@ public class EnderecoERPBean {
     private String lastname;
     private String firstname;
     private String address1;
+    private String numero;
     private String address2;
     private String postcode;
     private String city;
@@ -23,6 +24,18 @@ public class EnderecoERPBean {
     private String phone;
     private String phone_mobile;
     private String id_state;
+    private String enderecoCob;
+    private String numeroCob;
+    private String bairroCob;
+    private String cidadeCob;
+    private String estadoCob;
+    private String cepCob;
+    private String enderecoEnt;
+    private String numeroEnt;
+    private String bairroEnt;
+    private String cidadeEnt;
+    private String estadoEnt;
+    private String cepEnt;    
 
     public String getId_customer() {
         return id_customer;
@@ -62,6 +75,14 @@ public class EnderecoERPBean {
 
     public void setAddress1(String address1) {
         this.address1 = address1;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
     public String getAddress2() {
@@ -118,6 +139,102 @@ public class EnderecoERPBean {
 
     public void setId_state(String id_state) {
         this.id_state = id_state;
+    }
+
+    public String getEnderecoCob() {
+        return enderecoCob;
+    }
+
+    public void setEnderecoCob(String enderecoCob) {
+        this.enderecoCob = enderecoCob;
+    }
+
+    public String getNumeroCob() {
+        return numeroCob;
+    }
+
+    public void setNumeroCob(String numeroCob) {
+        this.numeroCob = numeroCob;
+    }
+
+    public String getBairroCob() {
+        return bairroCob;
+    }
+
+    public void setBairroCob(String bairroCob) {
+        this.bairroCob = bairroCob;
+    }
+
+    public String getCidadeCob() {
+        return cidadeCob;
+    }
+
+    public void setCidadeCob(String cidadeCob) {
+        this.cidadeCob = cidadeCob;
+    }
+
+    public String getEstadoCob() {
+        return estadoCob;
+    }
+
+    public void setEstadoCob(String estadoCob) {
+        this.estadoCob = estadoCob;
+    }
+
+    public String getCepCob() {
+        return cepCob;
+    }
+
+    public void setCepCob(String cepCob) {
+        this.cepCob = cepCob;
+    }
+
+    public String getEnderecoEnt() {
+        return enderecoEnt;
+    }
+
+    public void setEnderecoEnt(String enderecoEnt) {
+        this.enderecoEnt = enderecoEnt;
+    }
+
+    public String getNumeroEnt() {
+        return numeroEnt;
+    }
+
+    public void setNumeroEnt(String numeroEnt) {
+        this.numeroEnt = numeroEnt;
+    }
+
+    public String getBairroEnt() {
+        return bairroEnt;
+    }
+
+    public void setBairroEnt(String bairroEnt) {
+        this.bairroEnt = bairroEnt;
+    }
+
+    public String getCidadeEnt() {
+        return cidadeEnt;
+    }
+
+    public void setCidadeEnt(String cidadeEnt) {
+        this.cidadeEnt = cidadeEnt;
+    }
+
+    public String getEstadoEnt() {
+        return estadoEnt;
+    }
+
+    public void setEstadoEnt(String estadoEnt) {
+        this.estadoEnt = estadoEnt;
+    }
+
+    public String getCepEnt() {
+        return cepEnt;
+    }
+
+    public void setCepEnt(String cepEnt) {
+        this.cepEnt = cepEnt;
     }
     
     
