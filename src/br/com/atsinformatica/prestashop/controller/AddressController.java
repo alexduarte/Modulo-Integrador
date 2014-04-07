5cb2311db87b2198ccf6bc12126133d42cb60886
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-07 17:53:58 -0300

    Implementando Logger

--- src/br/com/atsinformatica/prestashop/controller/AddressController.java
+++ src/br/com/atsinformatica/prestashop/controller/AddressController.java
@@ -8,6 +8,7 @@ package br.com.atsinformatica.prestashop.controller;
 import br.com.atsinformatica.erp.entity.EnderecoERPBean;
 import br.com.atsinformatica.prestashop.clientDAO.AddressPrestashopDAO;
 import br.com.atsinformatica.prestashop.model.root.Address;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -15,40 +16,46 @@ import br.com.atsinformatica.prestashop.model.root.Address;
  */
 public class AddressController {
 
+    private static Logger logger = Logger.getLogger(AddressController.class);
+
     public EnderecoERPBean syncAddressControllerPrestashop(int codEntrega, int codCobranca) {
         AddressPrestashopDAO dao = new AddressPrestashopDAO();
 
         Address address = dao.getId(Address.URLADDRESS, codEntrega);
-        
-
         EnderecoERPBean bean = new EnderecoERPBean();
-        bean.setId_customer(address.getId_customer());
-        bean.setAlias(address.getAlias());
-        bean.setLastname(address.getLastname());
-        bean.setFirstname(address.getFirstname());
-        bean.setAddress1(address.getAddress1());
-        bean.setNumero(address.getNumero());
-        bean.setAddress2(address.getAddress2());
-        bean.setPostcode(address.getPostcode());
-        bean.setCity(address.getCity());
-        bean.setOther(address.getOther());
-        bean.setPhone(address.getPhone());
-        bean.setPhone_mobile(address.getPhone_mobile());
-        bean.setId_state(address.getId_state());
-        
-        /*
-        Buscando Endereço de cobrança
-        */
-        Address addressCobraca = dao.getId(Address.URLADDRESS, codCobranca);
-        
-        bean.setEnderecoCob(addressCobraca.getAddress1());
-        bean.setNumeroCob(addressCobraca.getNumero());
-        bean.setBairroCob(addressCobraca.getAddress2());
-        bean.setCepCob(addressCobraca.getPostcode());
-        bean.setCidadeCob(addressCobraca.getCity());
-        bean.setEstadoCob(addressCobraca.getId_state());
-        
-        
+        try {
+
+            bean.setId_customer(address.getId_customer());
+            bean.setAlias(address.getAlias());
+            bean.setLastname(address.getLastname());
+            bean.setFirstname(address.getFirstname());
+            bean.setAddress1(address.getAddress1());
+            bean.setNumero(address.getNumero());
+            bean.setAddress2(address.getAddress2());
+            bean.setPostcode(address.getPostcode());
+            bean.setCity(address.getCity());
+            bean.setOther(address.getOther());
+            bean.setPhone(address.getPhone());
+            bean.setPhone_mobile(address.getPhone_mobile());
+            bean.setId_state(address.getId_state());
+
+            /*
+             Buscando Endereço de cobrança
+             */
+            Address addressCobraca = dao.getId(Address.URLADDRESS, codCobranca);
+
+            bean.setEnderecoCob(addressCobraca.getAddress1());
+            bean.setNumeroCob(addressCobraca.getNumero());
+            bean.setBairroCob(addressCobraca.getAddress2());
+            bean.setCepCob(addressCobraca.getPostcode());
+            bean.setCidadeCob(addressCobraca.getCity());
+            bean.setEstadoCob(addressCobraca.getId_state());
+
+        } catch (Exception e) {
+            logger.error("Erro ao consultar endereço Entrega ID Ecom:(" + codEntrega + "), endereço cobrança ID Ecom:(" + codCobranca + "): " + e);
+            return null;
+        }
+
         return bean;
 
     }
