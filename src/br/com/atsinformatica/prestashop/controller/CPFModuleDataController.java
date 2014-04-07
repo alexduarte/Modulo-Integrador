4a34c643e1fdc3fbd779d8086af1e3af69a5d0f3
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-07 17:50:05 -0300

    Implementando Logger

--- src/br/com/atsinformatica/prestashop/controller/CPFModuleDataController.java
+++ src/br/com/atsinformatica/prestashop/controller/CPFModuleDataController.java
@@ -8,29 +8,33 @@ package br.com.atsinformatica.prestashop.controller;
 import br.com.atsinformatica.erp.entity.CPFClienteBean;
 import br.com.atsinformatica.prestashop.clientDAO.CPFModuleDataPrestashopDAO;
 import br.com.atsinformatica.prestashop.model.root.CPFModuleData;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author kennedimalheiros
  */
 public class CPFModuleDataController {
-
+    private static Logger logger = Logger.getLogger(CPFModuleDataController.class);
     public CPFClienteBean sysncCPDModuleData(int idCliente) {
         CPFModuleDataPrestashopDAO dao = new CPFModuleDataPrestashopDAO();
-
-        CPFModuleData cPFModuleData = dao.getId(CPFModuleData.URLCPFModuleData, idCliente);
         CPFClienteBean bean = new CPFClienteBean();
+        try {
+            CPFModuleData cPFModuleData = dao.getId(CPFModuleData.URLCPFModuleData, idCliente);
 
-        bean.setIdCLiente(cPFModuleData.getId_customer());
-        bean.setCpf_Cnpj(cPFModuleData.getDoc());
-        bean.setRg_inscricao(cPFModuleData.getIdt());
+            bean.setIdCLiente(cPFModuleData.getId_customer());
+            bean.setCpf_Cnpj(cPFModuleData.getDoc());
+            bean.setRg_inscricao(cPFModuleData.getIdt());
 
-        if (cPFModuleData.getType().equals("cpf")) {
-            bean.setTipoDocumento("F");
-        } else if (cPFModuleData.getType().equals("cnpj")) {
-            bean.setTipoDocumento("J");
+            if (cPFModuleData.getType().equals("cpf")) {
+                bean.setTipoDocumento("F");
+            } else if (cPFModuleData.getType().equals("cnpj")) {
+                bean.setTipoDocumento("J");
+            }
+
+        } catch (Exception e) {
+            logger.error("Erro ao consultar numero CPF/CNPJ do Cliente ID Ecom:(" + idCliente + "): " + e);
         }
-
         return bean;
 
     }
