ec4658d2670f7c548f9c5d2f29e28a26aaed808d
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-07 18:00:07 -0300

    Commit Inicial

---
+++ src/br/com/atsinformatica/prestashop/model/root/Carrier.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.atsinformatica.prestashop.model.root;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *
+ * @author kennedimalheiros
+ */
+@XmlRootElement(name = "carrier")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "carrier", propOrder = {
+    "id",
+    "name",// Nome transportadora    
+})
+public class Carrier {
+
+    public static String URLCARRIER = "carriers/";
+
+    @XmlElement(name = "id", required = true)
+    private String id;
+    @XmlElement(name = "name")
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
