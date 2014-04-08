0785de0541e4d3b6d39f2fd3d4b2373647a6d057
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-08 08:52:04 -0300

    Implementando Logger

--- src/br/com/atsinformatica/prestashop/controller/StateController.java
+++ src/br/com/atsinformatica/prestashop/controller/StateController.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.atsinformatica.prestashop.controller;
 
 import br.com.atsinformatica.erp.entity.EstadoERPBean;
 import br.com.atsinformatica.prestashop.clientDAO.StatesPrestashopDAO;
 import br.com.atsinformatica.prestashop.model.root.State;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -16,25 +16,30 @@ import br.com.atsinformatica.prestashop.model.root.State;
  */
 public class StateController {
 
+    private static Logger logger = Logger.getLogger(StateController.class);
+
     public EstadoERPBean syncStateControllerPrestashop(int codEndereco, int codEnderecoCobranca) {
         StatesPrestashopDAO dao = new StatesPrestashopDAO();
-
-        State state = dao.getId(State.URLSTATE, codEndereco);
-
         EstadoERPBean bean = new EstadoERPBean();
-        bean.setId(state.getId());
-        bean.setSigla(state.getIso_code());
-        bean.setDescricao(state.getName());
-        
-        State stateCobranca = dao.getId(State.URLSTATE, codEnderecoCobranca);
 
-        bean.setSiglaCobracao(state.getIso_code());
-        bean.setDescricaoCobracao(state.getName());
-        
-        
+        try {
+            State state = dao.getId(State.URLSTATE, codEndereco);
+
+            bean.setId(state.getId());
+            bean.setSigla(state.getIso_code());
+            bean.setDescricao(state.getName());
+
+            State stateCobranca = dao.getId(State.URLSTATE, codEnderecoCobranca);
+
+            bean.setSiglaCobracao(state.getIso_code());
+            bean.setDescricaoCobracao(state.getName());
+            logger.info("Buscando Estado do Endereço do cliente da loja Prestashop com sucesso.");
+        } catch (Exception e) {
+            logger.error("Erro ao Buscar Estado do Endereço do cliente da loja Prestashop: " + e);
+        }
+
         return bean;
 
-    }   
-     
-    
+    }
+
 }
