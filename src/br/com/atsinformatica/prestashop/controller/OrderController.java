3af2b5a3596002929ea6e149ebcec41edb30e550
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-08 08:43:38 -0300

    Implementando Logger

--- src/br/com/atsinformatica/prestashop/controller/OrderController.java
+++ src/br/com/atsinformatica/prestashop/controller/OrderController.java
@@ -27,6 +27,8 @@ import java.util.logging.Logger;
  */
 public class OrderController {
 
+    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(OrderController.class);
+
     public PedidoCERPBean syncOrderControllerPrestashop(int cod) {
         OrderPrestashopDAO dao = new OrderPrestashopDAO();
 
@@ -57,72 +59,81 @@ public class OrderController {
         PedidoIERPBean beanItens = null;
         List<PedidoCERPBean> lista = new ArrayList<>();
         List<PedidoIERPBean> listItens = new ArrayList<>();
-        List<Order> listOrder = dao.get(Order.URLORDER);
 
-        List<OrderRowNode> listOrderRowNode = new ArrayList<>();
-        /*
-         qtdRegistroSinc tem a quantidade de registro a sincronizar configurado.
-         */
-        int qtdRegistroSinc = new ParaEcomDAO().listaTodos().get(0).getQtdeRegistros();
-        int coutOrdersAdd = 0;
-        for (Order order : listOrder) {
+        try {
 
-            bean = new PedidoCERPBean();
-            if (order != null) {
+            List<Order> listOrder = dao.get(Order.URLORDER);
 
-                if (order.getId() != null) {
-                    //Return True se Existir, Não faz nada.
-                    //       False se não existir, Adiciona a Lista.                    
-                    if (!pedidoERPDAO.verificarPedidoEcomExisteERP(order.getId())) {
-                        bean.setId_ecom(order.getId());
-                        bean.setId_address_delivery(order.getId_address_delivery());
-                        bean.setId_address_invoice(order.getId_address_invoice());
-                        bean.setId_customer(order.getId_customer());
-                        bean.setId_carrier(order.getId_carrier());
-                        bean.setCurrent_state(order.getCurrent_state());
-                        bean.setModule(order.getModule());
-                        bean.setInvoice_number(order.getInvoice_number());
-                        bean.setInvoice_date(order.getInvoice_date());
-                        bean.setDelivery_date(order.getDelivery_date());
+            List<OrderRowNode> listOrderRowNode = new ArrayList<>();
+            /*
+             qtdRegistroSinc tem a quantidade de registro a sincronizar configurado.
+             */
+            int qtdRegistroSinc = new ParaEcomDAO().listaTodos().get(0).getQtdeRegistros();
+            int coutOrdersAdd = 0;
+            for (Order order : listOrder) {
 
-                        try {
-                            Date dataPedido;
+                bean = new PedidoCERPBean();
+                if (order != null) {
 
-                            dataPedido = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(order.getDate_add());
-                            bean.setDate_add(dataPedido);
+                    if (order.getId() != null) {
+                        //Return True se Existir, Não faz nada.
+                        //       False se não existir, Adiciona a Lista.                    
+                        if (!pedidoERPDAO.verificarPedidoEcomExisteERP(order.getId())) {
+                            bean.setId_ecom(order.getId());
+                            bean.setId_address_delivery(order.getId_address_delivery());
+                            bean.setId_address_invoice(order.getId_address_invoice());
+                            bean.setId_customer(order.getId_customer());
+                            bean.setId_carrier(order.getId_carrier());
+                            bean.setCurrent_state(order.getCurrent_state());
+                            bean.setModule(order.getModule());
+                            bean.setInvoice_number(order.getInvoice_number());
+                            bean.setInvoice_date(order.getInvoice_date());
+                            bean.setDelivery_date(order.getDelivery_date());
 
-                            String horaPedido = new SimpleDateFormat("HH:mm:ss").format(dataPedido);
-                            bean.setHora(horaPedido);
+                            try {
+                                Date dataPedido;
 
-                        } catch (ParseException ex) {
-                            Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
+                                dataPedido = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(order.getDate_add());
+                                bean.setDate_add(dataPedido);
+
+                                String horaPedido = new SimpleDateFormat("HH:mm:ss").format(dataPedido);
+                                bean.setHora(horaPedido);
+
+                            } catch (ParseException ex) {
+                                Logger.getLogger(OrderController.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+
+                            bean.setPayment(order.getPayment());
+                            bean.setTotal_discounts(order.getTotal_discounts());
+                            bean.setTotal_paid(order.getTotal_paid());
+                            bean.setTotal_paid_real(order.getTotal_paid_real());
+                            bean.setTotal_products(order.getTotal_products());
+                            bean.setTotal_shipping(order.getTotal_shipping());
+                            bean.setReference(order.getReference());
+
+                            listOrderRowNode = order.getAssociations().getOrderRowsNode().getOrderRow();
+                            bean.setListItensPedido(listOrderRowNode);
+                            /*
+                             (lista.add(bean) Adiciona o bean a lista a sincronizar.
+                             (coutOrdersAdd += 1) incrementa o contador de bean 
+                             O IF verifica se já atingiu o limite de registro a ser sincronizado.
+                             */
+                            lista.add(bean);
+                            coutOrdersAdd += 1;
+                            if (coutOrdersAdd >= qtdRegistroSinc) {
+                                break;
+                            }
+
                         }
-
-                        bean.setPayment(order.getPayment());
-                        bean.setTotal_discounts(order.getTotal_discounts());
-                        bean.setTotal_paid(order.getTotal_paid());
-                        bean.setTotal_paid_real(order.getTotal_paid_real());
-                        bean.setTotal_products(order.getTotal_products());
-                        bean.setTotal_shipping(order.getTotal_shipping());
-                        bean.setReference(order.getReference());
-
-                        listOrderRowNode = order.getAssociations().getOrderRowsNode().getOrderRow();
-                        bean.setListItensPedido(listOrderRowNode);
-                        /*
-                        (lista.add(bean) Adiciona o bean a lista a sincronizar.
-                        (coutOrdersAdd += 1) incrementa o contador de bean 
-                        O IF verifica se já atingiu o limite de registro a ser sincronizado.
-                        */
-                        lista.add(bean);
-                        coutOrdersAdd += 1;
-                        if (coutOrdersAdd >= qtdRegistroSinc) {
-                            break;
-                        }
-
                     }
                 }
             }
+            logger.info("Buscando Pedidos da loja Prestashop com sucesso.");
+        } catch (Exception e) {
+            logger.error("Erro ao Buscar Pedidos da loja Prestashop: " + e);
+            return null;
         }
+
         return lista;
     }
 
