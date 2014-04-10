8d91d846e2f0b89a8c218f854720e914bdb8afe7
kennedimalheiros <kennedimalheiros@gmail.com>
2014-04-10 09:15:31 -0300

    Adicionando TryCatch

--- src/br/com/atsinformatica/midler/components/renderer/StatusPedidoCellRenderer.java
+++ src/br/com/atsinformatica/midler/components/renderer/StatusPedidoCellRenderer.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package br.com.atsinformatica.midler.components.renderer;
 
 import br.com.atsinformatica.utils.Funcoes;
@@ -11,13 +10,17 @@ import java.awt.Component;
 import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import org.apache.log4j.Logger;
 
 /**
- * Renderer Para alterar o códido do status por seu nome,
- * tem dependencia da classe Funcoes com o metodo Funcoes.getOrderStatus
+ * Renderer Para alterar o códido do status por seu nome, tem dependencia da
+ * classe Funcoes com o metodo Funcoes.getOrderStatus
+ *
  * @author kennedimalheiros
  */
 public class StatusPedidoCellRenderer extends DefaultTableCellRenderer {
+
+    private static Logger logger = Logger.getLogger(StatusPedidoCellRenderer.class);
 
     public StatusPedidoCellRenderer() {
         super();
@@ -29,12 +32,17 @@ public class StatusPedidoCellRenderer extends DefaultTableCellRenderer {
             boolean isSelected,
             boolean hasFocus,
             int row,
-            int column) {        
-        JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        
-        label.setText(Funcoes.getOrderStatus((String) value));        
-        
+            int column) {
+        try {
+            JLabel label = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+            label.setText(Funcoes.getOrderStatus((String) value));
+        } catch (Exception e) {
+            logger.error("Erro ao executar função StatusPedidoCellRenderer: " + e);
+            return null;
+        }
+
         return this;
     }
-    
+
 }
