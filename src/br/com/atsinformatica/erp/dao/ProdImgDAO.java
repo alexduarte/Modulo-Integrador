d360e0630a583e719a65f240798bd9c239cda208
AlexsanderPimenta <AlexsanderPimenta@ATS552233.atsmoc.local>
2014-04-14 16:25:54 -0300

    metodo que lista imagens pelo codigo do produto

--- src/br/com/atsinformatica/erp/dao/ProdImgDAO.java
+++ src/br/com/atsinformatica/erp/dao/ProdImgDAO.java
@@ -4,67 +4,54 @@
  */
 package br.com.atsinformatica.erp.dao;
 
+import br.com.atsinformatica.erp.entity.ImgProdBean;
+import br.com.atsinformatica.midler.jdbc.ConexaoATS;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+
 /**
  *
  * @author AlexsanderPimenta
- */
-//public class ProdImgDAO implements IGenericDAO<ProdImgBean> {
+ */ 
+public class ProdImgDAO {
+    private Connection conn;
     
-    public class ProdImgDAO {
-//    private Logger logger = Logger.getLogger(ProdImgDAO.class);
-//    private Connection conn;
-//
-//    @Override
-//    public void gravar(ProdImgBean object) throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void alterar(ProdImgBean object) throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void deletar(String id) throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public ProdImgBean abrir(String id) throws SQLException {
-//        PreparedStatement pstmt = null;
-//        ResultSet rs = null;
-//        try{
-//            conn = ConexaoATS.conectaERP();
-//            String querie = "select * from prodimg where prodimg.codprod = ?";
-//            pstmt = conn.prepareStatement(querie);
-//            pstmt.setString(1, id);
-//            rs = pstmt.executeQuery();
-//            ProdImgBean bean = null;
-//            while(rs.next()){
-//                bean = new ProdImgBean();
-//                bean.setCodProd(rs.getString("codprod"));
-//                bean.setFoto(rs.getBytes("foto"));                
-//            }
-//            logger.info("Imagens do produto retornadas com sucesso!");
-//            return bean;
-//        }catch(Exception e){
-//            logger.error("Erro ao retornar imagens do produto: "+e);            
-//            return null;
-//        }finally{
-//            rs.close();
-//            pstmt.close();
-//            conn.close();
-//        }   
-//    }
-//
-//    @Override
-//    public List<ProdImgBean> listaTodos() throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public String ultimoRegistro() throws SQLException {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
+    
+    /**
+     * Retorna url de imagens do produto
+     * @param codProd
+     * @return List<ImgProdBean>
+     */
+    public List<ImgProdBean> listaImgCodProd(String codProd){
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<ImgProdBean> listaImgProdBean = new ArrayList<>();
+        try{
+            conn = ConexaoATS.conectaERP();
+            String sql = "SELECT CODPROD, URLIMAGEM FROM IMGPRODECOMMERCE       "
+                       + "WHERE CODPROD = ?                                     ";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, codProd);
+            rs = pstmt.executeQuery();
+            ImgProdBean imgProdBean = null;
+            while(rs.next()){
+                imgProdBean = new ImgProdBean();
+                imgProdBean.setCodProd(rs.getString("codprod"));
+                imgProdBean.setUrlImagem(rs.getString("urlimagem"));
+                listaImgProdBean.add(imgProdBean);
+            }
+            Logger.getLogger(ProdImgDAO.class).info("Url de imagens do produto, retornada com sucesso!");
+            return listaImgProdBean;
+        }catch(Exception e){
+            Logger.getLogger(ProdImgDAO.class).error("Erro ao retornar url de imagens do produto: "+e);
+            return null;
+            
+        }
+    }
     
 }
